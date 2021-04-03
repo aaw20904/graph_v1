2365eecc33e5e1d23d101a22c404d36b885bbc8a
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2021-04-03 20:40:18 +0300

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,15 +1,15 @@
 let Hystogram = (function () {
   let mapAccess = new WeakMap();
    let secM = {
-     clearCanvas: function (settings) {
+     clearCanvas: function () {
       /*get a context*/ 
-      settings.canvasCtx.clearRect(0, 0, settings.limits.w, settings.limits.w);              
+      this.settings.canvasCtx.clearRect(0, 0, this.settings.limits.w, this.settings.limits.w);              
      },
        /*paint a scale on the Canvas-
        maxX - maximum value of argument (reserved for latest version)
        maxY - maximum value of the function*/
-     paintScale: function (maxX, maxY, privM) {
-       let settings = privM.settings;
+     paintScale: function () {
+       let settings = this.settings;
        let offset = 0;
        let scaleVal = 0;
        /*all the size in pixels */
@@ -31,34 +31,35 @@ let Hystogram = (function () {
           settings.canvasCtx.lineWidth = 2;
           settings.canvasCtx.beginPath ();
           /*Y axis*/
-          privM.moveToDekart(startPoint.x, startPoint.y, settings);
-          privM.lineToDekart(startPoint.x + (settings.limits.w * settings.diagramArea) , startPoint.y, settings);
+          this.moveToDekart(startPoint.x, startPoint.y, settings);
+          this.lineToDekart(startPoint.x + (settings.limits.w * settings.diagramArea) , startPoint.y, settings);
          
           /* X axis*/
-          privM.moveToDekart(startPoint.x, startPoint.y, settings);
-          privM.lineToDekart(startPoint.x, startPoint.y + (settings.limits.h * settings.diagramArea), settings);
+          this.moveToDekart(startPoint.x, startPoint.y, settings);
+          this.lineToDekart(startPoint.x, startPoint.y + (settings.limits.h * settings.diagramArea), settings);
           scaleVal = stepOfScale;
           /*a vertical scale*/
           while (tmp < 4) {
             offset += (settings.diagramArea * settings.limits.h) / 4;
-            privM.moveToDekart (startPoint.x * 0.9, startPoint.y + offset, settings);
+            this.moveToDekart (startPoint.x * 0.9, startPoint.y + offset);
               /*paint a text*/
-            privM.textDekart (scaleVal.toFixed(), (settings.limits.w * 0.02) ,startPoint.y + offset, settings.limits.w * ((1-settings.diagramArea)/2), settings);
+            this.textDekart (scaleVal.toFixed(), (settings.limits.w * 0.02) ,startPoint.y + offset, settings.limits.w * ((1-settings.diagramArea)/2));
             /*paint a short line-like a scale*/
-            privM.moveToDekart (startPoint.x * 0.9, startPoint.y + offset, settings);
-            privM.lineToDekart (startPoint.x, startPoint.y + offset, settings);
+            this.moveToDekart (startPoint.x * 0.9, startPoint.y + offset);
+            this.lineToDekart (startPoint.x, startPoint.y + offset);
             tmp++;
             scaleVal += stepOfScale;
           }
           settings.countOfItems = 4;
 
        //   privM.paintItem(0,50,"red","@",privM);
-          settings.canvasCtx.stroke ();
+          this.settings.canvasCtx.stroke ();
      },
 
      /*update a private members */ 
-    updateWindowLimits: function (settings) {
-      console.log(this.settings);
+    updateWindowLimits: function () {
+      let settings = this.settings;
+      console.log(this.settings.limits.w);
        /*save  window settings in a private settings object*/
        settings.limits.w = window.innerWidth;
        settings.limits.h = window.innerWidth * 0.66; 
@@ -72,9 +73,9 @@ let Hystogram = (function () {
     /*@order - means a order on a diagram
     rorExample: if countOfItems equals 10, THEN 
     the order can be in range from 0 to 10*/
-    paintItem (order, parameter, color, letter, privM) {
+    paintItem (order, parameter, color, letter) {
       
-        let settings = privM.settings;
+        let settings = this.settings;
         settings.canvasCtx.fillStyle = color;
         let step = (settings.limits.w * settings.diagramArea) / settings.countOfItems;
         let hightRect = (parameter / settings.maxValY) * (settings.limits.h  * settings.diagramArea);
@@ -84,12 +85,12 @@ let Hystogram = (function () {
           /*assign a concrete value in pixels to the start point*/
           startPoint.x *= settings.limits.w;
           startPoint.y *= settings.limits.h;
-          privM.fillRectDekart(startPoint.x + (step * order), startPoint.y, step, hightRect, privM);
+          this.fillRectDekart(startPoint.x + (step * order), startPoint.y, step, hightRect);
           /*paint the letter*/
           let letterHeigh  = settings.limits.h * 0.05;
           let letterY = startPoint.y - (settings.limits.h * 0.08);
           let letterX = (startPoint.x + (step * order)) + (step / 2); 
-          privM.textDekart(letter, letterX, letterY, step, settings );
+          this.textDekart(letter, letterX, letterY, step );
           settings.canvasCtx.fillStyle = settings.color;
         },
         /*paint functins - LOWER LEFT corner equals ZERO*/
@@ -100,20 +101,20 @@ let Hystogram = (function () {
            result.x = x;
           return result;
          },
-          lineToDekart: function (x, y, settings) {
-           settings.canvasCtx.lineTo(x, settings.limits.h - y); 
+          lineToDekart: function (x, y) {
+           this.settings.canvasCtx.lineTo(x, this.settings.limits.h - y); 
           },
-          moveToDekart: function (x, y, settings) {
-            settings.canvasCtx.moveTo(x, settings.limits.h - y); 
+          moveToDekart: function (x, y) {
+            this.settings.canvasCtx.moveTo(x, this.settings.limits.h - y); 
           },
-          fillRectDekart: function (x, y, w, h, settings) {
-            settings.canvasCtx.fillRect(x, settings.limits.h - y, w, h); 
+          fillRectDekart: function (x, y, w, h) {
+            this.settings.canvasCtx.fillRect(x, this.settings.limits.h - y, w, h); 
           },
-          textDekart: function (text, x, y, maxWidth, settings) {
-            settings.canvasCtx.fillText(text, x, settings.limits.h - y, maxWidth); 
+          textDekart: function (text, x, y, maxWidth) {
+            this.settings.canvasCtx.fillText(text, x, this.settings.limits.h - y, maxWidth); 
           },
-          fillRectDekart: function (x,y,w,h,privM) {
-           privM.settings.canvasCtx.fillRect(x, privM.settings.limits.h - (y + h), w, h); 
+          fillRectDekart: function (x,y,w,h) {
+           this.settings.canvasCtx.fillRect(x, this.settings.limits.h - (y + h), w, h); 
           },
     /*****settings of a Canvas view */
     /*in  general there are a ratio coeficients*/ 
@@ -158,14 +159,14 @@ let Hystogram = (function () {
        
          updateCanvasView(){
           let priv = mapAccess.get(this);
-          priv.updateWindowLimits(priv.settings);
-          priv.clearCanvas(priv.settings);
+          priv.updateWindowLimits();
+          priv.clearCanvas();
 
          }
 
          paintHystogram (dataSet, maxValue) {
             let privM = mapAccess.get(this);
-            privM.paintScale(0,0,privM);
+            privM.paintScale();
             /*max value of the function*/
             privM.settings.maxValY = maxValue;
             /*number of counts*/
@@ -177,7 +178,7 @@ let Hystogram = (function () {
             let propResult  = propIter.next(); 
             privM.settings.canvasCtx.beginPath();
             while (!dataResult.done) {
-              privM.paintItem(count, dataResult.value, propResult.value[0].color, propResult.value[0].letter, privM);
+              privM.paintItem(count, dataResult.value, propResult.value[0].color, propResult.value[0].letter);
               dataResult = dataIter.next();
               propResult  = propIter.next();
               count++;
@@ -199,6 +200,8 @@ let Hystogram = (function () {
        base.add(99);
        base.add(51);
        base.add(27);
+       base.add(15);
+     
        q.updateCanvasView();
        q.paintHystogram(base, 100);
        window.addEventListener("resize",()=>{
